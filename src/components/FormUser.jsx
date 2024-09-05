a4d743b84342d36d535c6b08b9228168efd6d2d3
andreaandrademejia <151689497+andreaandrademejia@users.noreply.github.com.>
2024-09-05 16:20:39 -0600

    update date

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,102 +1,192 @@
-import { useForm } from "react-hook-form";
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import './styles/FormUser.css';
+import Swal from 'sweetalert2';
 
-const FormUser = () => {
+const FormUser = ({
+	createUser,
+	userSelected,
+	setUserSelected,
+	updateUser,
+	setFormIsOpen,
+	formIsOpen,
+}) => {
+	const {
+		handleSubmit,
+		register,
+		reset,
+		formState: { errors },
+	} = useForm({ mode: 'onChange' });
 
- const { handleSubmit, register, reset} = useForm();
+	useEffect(() => {
+		reset(userSelected);
+	}, [userSelected]);
 
-const submit = (data) => {
-	createUser(data);
-	reset({
-		email: '',
-    password: '',
-    first_Name: '',
-    last_Name: '',
-    birthday: '',
-    image_url: '',
-	});
-};
+	const submit = (data) => {
+		if (userSelected) {
+			updateUser(userSelected.id, data);
+			setUserSelected();
+			Swal.fire({
+				title: 'User Updated!',
+				text: `User ${userSelected.first_name} ${userSelected.last_name} has been updated!`,
+				icon: 'success',
+			});
+		} else {
+			createUser(data);
+			Swal.fire({
+				title: 'User Created!',
+				text: `User ${data.first_name} ${data.last_name} has been Created!`,
+				icon: 'success',
+			});
+		}
+
+		reset({
+			email: '',
+			password: '',
+			first_name: '',
+			last_name: '',
+			birthday: '',
+			image_url: '',
+		});
+
+		setFormIsOpen(false);
+	};
+
+	const handleExit = () => {
+		setFormIsOpen(false);
+		reset({
+			email: '',
+			password: '',
+			first_name: '',
+			last_name: '',
+			birthday: '',
+			image_url: '',
+		});
+		setUserSelected();
+	};
 
 	return (
-		<div>
-‌<form onSubmit={handleSubmit(submit)}>
-‌			<label>
-‌          <span>Email</span>
-​  <input
-​    type="email"
-​    {...register('email', {
-​      maxLength: {
-​        value: 50,
-​        message: 'The maximum number of characters is 50',
-​      },
-​      required: 'This field is required',
-​    })}
-​  />
-​  <p>{errors.email?.message}</p>
-‌        </label>
-‌					<span>Password</span>
-‌					<input type="password"
-‌					{...register('password', {
-‌              maxLength: {
-‌                value: 20,
-‌                message: 'The maximum number of characters is 20',
-‌              },
-‌              required: 'This field is required',
-‌            })}
-              />
-‌          <p>{errors.password?.message}</p>
-‌				</labe>
-				<label>
-					<span>First Name</span>
-					<input type="text" />
-					{...register('first_name', {
-              maxLength: {
-                value: 15,
-                message: 'The maximum number of characters is 15',
-              },
-              required: 'This field is required',
-            })}
-          />
-          <p>{errors.first_name?.message}</p>
-				</label>
-				<label>
-					<span>Last Name</span>
-					<input type="text" />
-					{...register('last_name', {
-              maxLength: {
-                value: 15,
-                message: 'The maximum number of characters is 15',
-              },
-              required: 'This field is required',
-            })}
-          <p>{errors.last_name?.message}</p>
-				</label>
-				<label>
-          <span>Birthday</span>
-          <input
-            type="date"
-            {...register('birthday', {
-              maxLength: {
-                value: 10,
-                message: 'The maximum number of characters is 10',
-              },
-              required: 'This field is required',
-            })}
-          />
-          <p>{errors.birthday?.message}</p>
-        </label>
-				<label>
-					<span>Image Url</span>
-					<input type="text" />
-					{...register('image_url', {
-              maxLength: {
-                value: 70,
-                message: 'The maximum number of characters is 70',
-              },
-              required: 'This field is required',
-            })}
-          <p>{errors.image_url?.message}</p>
-				</label>
-        <button>Submit</button>
+		<div
+			className={`form__container flex-container ${
+				formIsOpen || 'form__close'
+			}`}
+		>
+			<form onSubmit={handleSubmit(submit)} className="form flex-container">
+				<span className="form__exit flex-container" onClick={handleExit}>
+					<i className="bx bx-x"></i>
+				</span>
+				<h2 className="form__title">
+					{userSelected ? 'Update User Form' : 'Create User Form'}
+				</h2>
+				<div className="form__list">
+					<label className="form__field grid-container">
+						<span className="form__label">Email</span>
+						<input
+							className={`form__input ${
+								errors.email ? 'form__input--error' : ''
+							}`}
+							type="email"
+							{...register('email', {
+								maxLength: {
+									value: 50,
+									message: 'The maximum number of characters is 50',
+								},
+								required: 'This field is required',
+							})}
+							autoComplete="email"
+						/>
+						<p className="form__error">{errors.email?.message}</p>
+					</label>
+					<label className="form__field grid-container">
+						<span className="form__label">Password</span>
+						<input
+							className={`form__input ${
+								errors.password ? 'form__input--error' : ''
+							}`}
+							type="password"
+							{...register('password', {
+								maxLength: {
+									value: 20,
+									message: 'The maximum number of characters is 20',
+								},
+								required: 'This field is required',
+							})}
+							autoComplete="new-password"
+						/>
+						<p className="form__error">{errors.password?.message}</p>
+					</label>
+					<label className="form__field grid-container">
+						<span className="form__label">First Name</span>
+						<input
+							className={`form__input ${
+								errors.first_name ? 'form__input--error' : ''
+							}`}
+							type="text"
+							{...register('first_name', {
+								maxLength: {
+									value: 15,
+									message: 'The maximum number of characters is 15',
+								},
+								required: 'This field is required',
+							})}
+							autoComplete="given-name"
+						/>
+						<p className="form__error">{errors.first_name?.message}</p>
+					</label>
+					<label className="form__field grid-container">
+						<span className="form__label">Last Name</span>
+						<input
+							className={`form__input ${
+								errors.last_name ? 'form__input--error' : ''
+							}`}
+							type="text"
+							{...register('last_name', {
+								maxLength: {
+									value: 15,
+									message: 'The maximum number of characters is 15',
+								},
+								required: 'This field is required',
+							})}
+							autoComplete="family-name"
+						/>
+						<p className="form__error">{errors.last_name?.message}</p>
+					</label>
+					<label className="form__field grid-container">
+						<span className="form__label">Birthday</span>
+						<input
+							className={`form__input ${
+								errors.birthday ? 'form__input--error' : ''
+							}`}
+							type="date"
+							{...register('birthday', {
+								maxLength: {
+									value: 10,
+									message: 'The maximum number of characters is 10',
+								},
+								required: 'This field is required',
+							})}
+							autoComplete="bday"
+						/>
+						<p className="form__error">{errors.birthday?.message}</p>
+					</label>
+					<label className="form__field grid-container">
+						<span className="form__label">Image Url</span>
+						<input
+							className={`form__input ${
+								errors.image_url ? 'form__input--error' : ''
+							}`}
+							type="text"
+							{...register('image_url', {
+								required: 'This field is required',
+							})}
+							autoComplete="off"
+						/>
+						<p className="form__error">{errors.image_url?.message}</p>
+					</label>
+				</div>
+				<button className="form__btn">
+					{userSelected ? 'Update' : 'Create'}
+				</button>
 			</form>
 		</div>
 	);
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,63 @@
-const UserCard = ({ user }) => {
+import Swal from 'sweetalert2';
+import './styles/UserCard.css';
+
+const UserCard = ({ user, deleteUser, setUserSelected, setFormIsOpen }) => {
+	const handleDelete = () => {
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				deleteUser(user.id);
+				Swal.fire({
+					title: 'User Deleted!',
+					text: `User ${user.first_name} ${user.last_name} has been deleted.`,
+					icon: 'success',
+				});
+			}
+		});
+	};
+
+	const handleEdit = () => {
+		setUserSelected(user);
+		setFormIsOpen(true);
+	};
+
 	return (
-		<articule>
-			<img src={user.image_url} alt={'${user.first_name}${user.last_name}'} />
-			<h3>
+		<article className="user">
+			<img
+				className="user__img"
+				src={user.image_url}
+				alt={`${user.first_name} ${user.last_name}`}
+			/>
+			<h3 className="user__name">
 				{user.first_name} {user.last_name}
 			</h3>
-			<hr />
-			<ul>
-				<li>
-					<span>Email:</span>
-					<span>{user.email}</span>
+			<hr className="user__hr" />
+			<ul className="user__list grid-container">
+				<li className="user__item grid-container">
+					<span className="user__label">Email:</span>
+					<span className="user__value">{user.email}</span>
 				</li>
-				<li>
-					<span>Birthday</span>
-					<span>{user.birthday}</span>
+				<li className="user__item grid-container">
+					<span className="user__label">Birthday:</span>
+					<span className="user__value">{user.birthday}</span>
 				</li>
 			</ul>
-			<footer>
-				<button>Delete</button>
-				<button>Edit</button>
+			<footer className="user__footer flex-container">
+				<button onClick={handleDelete} className="user__btn user__delete">
+					Delete
+				</button>
+				<button onClick={handleEdit} className="user__btn user__edit">
+					Edit
+				</button>
 			</footer>
-		</articule>
+		</article>
 	);
 };
 
